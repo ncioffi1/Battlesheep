70fc52c178d5408e3de9b95160796ad56bf33d93
ncioffi1 <imnickcioffi@gmail.com>
2023-12-13 22:33:44 -0500

    commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+console.log("webpack confirmed working")
+
+const Game = require('./game.js');
+const GameView = require('./game-view.js');
+const Asteroid = require('./asteroid.js');
+const MovingObject = require('./moving-object.js');
+
+// let game = new Game() 
+document.addEventListener('DOMContentLoaded', () => {
+    // grab canvas
+    const canvas = document.getElementById("canvas");
+    canvas.width = 800;
+    canvas.height = 600;
+    const ctx = canvas.getContext('2d');
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+    const game = new Game(ctx);
+    const obj = new MovingObject([100, 100], [5,5], 20, "blue");
+    const gameview = new GameView(game, ctx);
+    gameview.start();
+    obj.draw(ctx);
+
+    // add code to test on window
+    window.Game = Game;
+    window.Asteroid = Asteroid;
+    window.MovingObject = MovingObject;
+})

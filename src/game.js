7258ae25100aa565c745f936ba6b4d0ca298b875
ncioffi1 <imnickcioffi@gmail.com>
2023-12-14 14:26:03 -0500

    commit

--- src/game.js
+++ src/game.js
@@ -1,66 +1,156 @@
 console.log("im the game")
 
-const Asteroid = require('./asteroid.js')
+var square = document.getElementById("square");
+
+// const Asteroid = require('./asteroid.js')
 
 class Game {
-    static DIM_X = 500;
-    static DIM_Y = 500;
-    static BG_COLOR = "black";
-    static NUM_ASTEROIDS = 5;
-
-    constructor(canvasCtx) {
-        this.canvasCtx = canvasCtx;
-        this.asteroids = [];
-        this.addAsteroids();
-    }
-
-    addAsteroids() {
-        for (let i = 0; i < Game.NUM_ASTEROIDS; i++) {
-            let pos = this.randomPosition();
-            console.log(pos);
-            let rock = new Asteroid(pos);
-            this.asteroids.push(rock);
-        }
-        this.draw(this.canvasCtx);
-    }
-
-    randomPosition() {
-        let x = this.getRandomInt(0, Game.DIM_X);
-        let y = this.getRandomInt(0, Game.DIM_Y);
-        let pos = [x, y]
-        return pos;
-    }
-
-    getRandomInt(min, max) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
-    }
-
-    draw(ctx) {
-        let ctx2 = this.canvasCtx;
-        ctx2.clearRect(0, 0, Game.DIM_X, Game.DIM_Y);
-        ctx2.fillStyle = Game.BG_COLOR;
-        ctx2.fillRect(0, 0, Game.DIM_X, Game.DIM_Y);
-
-        // this.allObjects().forEach(function(object) {
-        //     object.draw(ctx);
-        // })
-        // let ctx2 = this.canvasCtx;
-        this.asteroids.forEach(function(asteroid) {
-            asteroid.draw(ctx2);
-        })
-    }
-
-    moveObjects(){
-        this.asteroids.forEach(function(asteroid) {
-            asteroid.move();
-        })
+    constructor(ctx1, ctx2, ctx3, DIM_X, DIM_Y) {
+        this.i = 1;
+        // this.ctx = ctx;
+        this.ctx1 = ctx1;
+        this.ctx2 = ctx2;
+        this.ctx3 = ctx3;
+        this.state = 0;
+        this.altState = 0;
+        this.DIM_X = DIM_X;
+        this.DIM_Y = DIM_Y;
+        this.play = this.play.bind(this);
     }
 
     // allObjects() {
     //     return [].concat(this.ships, this.asteroids, this.bullets);
     // };
+
+    drawImageInCanvas(context, filepath, xPos, yPos, xSize, ySize) {
+        var img1 = new Image();
+        //drawing of the test image - img1
+        img1.onload = () => {
+            //draw background image
+            // console.log(img1.width);
+            // console.log(img1.height);
+
+            // this puts image in a spot with a centered anchorpoint.  
+            // if you don't want a centered anchorpoint, do this without subtracting x/ysize /2.
+            context.drawImage(img1, (xPos - xSize/2), (yPos - ySize/2), xSize, ySize);
+        };
+        img1.src = filepath;
+    }
+    // creating a custom game update loop.
+    // note that this will 
+    play() {
+        // console.log(this.i);
+        if (this.state === 0){
+            if (this.altState === 0) {
+                this.altState = 1;
+                // generate game assets
+                console.log("hello world");
+
+                this.ctx1.fillStyle = 'green';
+                this.ctx1.fillRect(0, 0, 800, 600)
+                
+                this.generateSquares(this.ctx3);
+                
+                this.ctx2.font = ("30px Arial");
+                this.ctx2.fillStyle = "black";
+                this.ctx2.textAlign = "center";
+                this.ctx2.fillText("Place Your Ships", 400, 50);
+                
+                // generate game board
+
+
+                // generate game images
+
+                // generate game ship objects
+
+                // game is in "place your ships" state
+
+                
+
+
+                console.log(this.altState);
+            } else if (this.altState === 1) {
+
+            }
+        }
+        this.i += 1;
+        // console.log(this.i);
+
+
+        // run the callback loop.
+        requestAnimationFrame(this.play);
+    }
+
+    generateSquares(ctx) {
+        // let square = ctx.add.sprite((0 + (1 * 100)), (0 + (1 * 100)), 'square');
+        // this.drawImageInCanvas(ctx, 'assets/square_outline.png', 400, 300, 50, 50);
+        let size = 7;
+        for (let i = 0; i < size; i++) {
+            for (let j = 0; j < 7; j++){
+                let xPos = 400 - ((Math.floor(size / 2)) * 50) + (i * 50);
+                let yPos = 200 + (j * 50);
+                let ship = new Ship(ctx, xPos, yPos, 50, 50, "blank");
+                // this.drawImageInCanvas(ctx, 'assets/square_outline.png', xPos, yPos, 50, 50);
+                
+                // let x_left = 278 + (i * 50);
+                // let x_right = 324 + (i * 50);
+                // let y_top = 176 + (j * 50);
+                // let y_bot = 223 + (j * 50);
+                // let button = new Button(x_left, x_right, y_top, y_bot, "board", [i, j])
+                // buttons.push(button);
+                // your_squares.push(square);
+            }
+        }
+    }
+
+    // clear(ctx) {
+
+    // }
+}
+
+class Ship {
+    constructor(context, xPos, yPos, xSize, ySize, color) {
+        this.context = context;
+        this.xPos = xPos;
+        this.yPos = yPos;
+        this.xSize = xSize;
+        this.ySize = ySize;
+        this.color = color;
+        this.draw = this.draw.bind(this)
+        this.draw();
+    }
+
+    changeColor(newColor) {
+        this.color = newColor;
+    }
+
+    draw() {
+        // this.context.clearRect(0, 0, 800, 600);
+        let filepath = ""
+        if (this.color === "blank"){
+            filepath = 'assets/square_outline.png'
+        } else if (this.color === "hover"){
+            filepath = 'assets/square_outline_transparent.png'
+        } else if (this.color === "white") {
+            filepath = 'assets/square_outline_white.png'
+        }
+
+        this.drawImageInCanvas(this.context, filepath, this.xPos, this.yPos, this.xSize, this.ySize)
+        // console.log("hello")
+
+        requestAnimationFrame(this.draw);
+    }
+
+    drawImageInCanvas(context, filepath, xPos, yPos, xSize, ySize) {
+        var img1 = new Image();
+        //drawing of the test image - img1
+        img1.onload = () => {
+            context.drawImage(img1, (xPos - xSize/2), (yPos - ySize/2), xSize, ySize);
+        };
+        img1.src = filepath;
+    }
+
+
 }
 
 // Game.prototype.randomPosition = function () {
--- src/index.js
+++ src/index.js
@@ -1,27 +1,38 @@
 console.log("webpack confirmed working")
 
 const Game = require('./game.js');
-const GameView = require('./game-view.js');
-const Asteroid = require('./asteroid.js');
-const MovingObject = require('./moving-object.js');
+// const GameView = require('./game-view.js');
+// const Asteroid = require('./asteroid.js');
+// const MovingObject = require('./moving-object.js');
 
 // let game = new Game() 
 document.addEventListener('DOMContentLoaded', () => {
     // grab canvas
-    const canvas = document.getElementById("canvas");
-    canvas.width = 800;
-    canvas.height = 600;
-    const ctx = canvas.getContext('2d');
-    ctx.fillStyle = 'black';
-    ctx.fillRect(0, 0, canvas.width, canvas.height)
-    const game = new Game(ctx);
-    const obj = new MovingObject([100, 100], [5,5], 20, "blue");
-    const gameview = new GameView(game, ctx);
-    gameview.start();
-    obj.draw(ctx);
+
+    // gonna try to do this with layers...
+    const layer1 = document.getElementById("layer1");
+    const layer2 = document.getElementById("layer2");
+    layer1.width = 800;
+    layer1.height = 600;
+    layer2.width = 800;
+    layer2.height = 600;
+    
+    const ctx1 = layer1.getContext('2d');
+    const ctx2 = layer2.getContext('2d');
+    const ctx3 = layer3.getContext('2d');
+    // ctx.fillStyle = 'green';
+    // ctx.fillRect(0, 0, canvas.width, canvas.height)
+    const game = new Game(ctx1, ctx2, ctx3, layer1.width, layer1.height);
+    game.play();
+    // const obj = new MovingObject([100, 100], [5,5], 20, "blue");
+    // const gameview = new GameView(game, ctx);
+    // gameview.start();
+    // obj.draw(ctx);
 
     // add code to test on window
-    window.Game = Game;
-    window.Asteroid = Asteroid;
-    window.MovingObject = MovingObject;
+    // window.Game = Game;
+    // window.Asteroid = Asteroid;
+    // window.MovingObject = MovingObject;
 })
+
+console.log("boop");
